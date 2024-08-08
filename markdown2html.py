916db7edd3658d02027e2861ddc9330081370fbe
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-08-08 18:21:58 +0100

    update

--- markdown2html.py
+++ markdown2html.py
@@ -12,17 +12,11 @@ def main():
               file=sys.stderr)
         sys.exit(1)
 
-    markdown_file = sys.argv[1]
-    html_file = sys.argv[2]
-
-    if not os.path.exists(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
+    if not os.path.isfile(sys.argv[1]):
+        print(f'Missing {sys.argv[1]}', file=sys.stderr)
         sys.exit(1)
     sys.exit(0)
 
 
 if __name__ == "__main__":
-    """
-    markdown2html module
-    """
     main()
